10de4262b3e2b38e2e72fb8c55ff31d98d214bae
zkindest <zkindest5@gmail.com>
2021-11-12 13:21:57 +0530

    add js interactity,improve code

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-var sidebar, stagePreview;
-window.onload = () => {
-  sidebar = document.getElementById('sidebar');
-  stagePreview = document.querySelector('.product__gallery .stage__img--active');
-  initGallery();
-}
+var sidebar = document.getElementById('sidebar');
+var galleryStageEl = document.querySelector('.jsPgStage');
+var galleryThumbnails = document.querySelectorAll(".jsPgItem");
+var galleryStagePrevBtn = document.querySelector('.jsPgStagePrev');
+var galleryStageNextBtn = document.querySelector('.jsPgStageNext');
+
 function openSidebar() {
   if (!sidebar.classList.contains('active')) {
     sidebar.classList.add('active')
@@ -16,23 +16,67 @@ function closeSidebar() {
   }
 }
 
-var thumbs;
+// GALLERY
+var galleryThumbnails;
 function initGallery() {
-  thumbs = document.querySelectorAll(".product__gallery .item");
 
   // disable nextPrevBtn if slide count = 1
-  if (thumbs.length < 2) {
-    document.querySelector('.product__gallery .stage__img--prev').style.display = "none";
-    document.querySelector('.product__gallery .stage__img--next').style.display = "none";
+  if (galleryThumbnails.length < 2) {
+    galleryStagePrevBtn.style.display = "none";
+    galleryStageNextBtn.style.display = "none";
+  } else {
+    galleryStagePrevBtn.setAttribute("onclick", "showGalleryPrevItem(this)")
+    galleryStageNextBtn.setAttribute("onclick", "showGalleryNextItem(this)")
+    galleryStagePrevBtn.style.visibility = "hidden";
   }
   // add listeners
-  thumbs.forEach((thumb) => {
-    thumb.setAttribute("onclick", "showFull(this)");
+  galleryThumbnails.forEach((thumb) => {
+    thumb.setAttribute("onclick", "previewGalleryItem(this)");
   })
 
+  // set inital data for stage
+  galleryStageEl.setAttribute("data-activeidx", "0");
 }
 
-function showFull(that) {
-  stagePreview.setAttribute("data-curr_img", that.dataset.fullimageurl);
-  stagePreview.firstElementChild.src = "../images/" + that.dataset.fullimageurl;
+function previewGalleryItem(that) {
+  if (that.dataset.fullimageurl === galleryStageEl.dataset.fullimageurl) {
+    return;
+  }
+  galleryStageEl.setAttribute("data-curr_img", that.dataset.fullimageurl);
+  galleryStageEl.setAttribute("data-activeidx", that.dataset.idx);
+
+  galleryStageEl.firstElementChild.src = "../images/" + that.dataset.fullimageurl;
+
+  setActiveThumbnail(galleryThumbnails, that)
 }
+
+function showGalleryNextItem() {
+  let currIdx = Number(galleryStageEl.dataset.activeidx);
+  let nextIdx = currIdx + 1;
+
+  // handle edge cases
+  if (currIdx === 0) {
+    galleryStagePrevBtn.style.visibility = "visible";
+  }
+  if (nextIdx === galleryThumbnails.length - 1) {
+    galleryStageNextBtn.style.visibility = "hidden";
+  }
+
+  previewGalleryItem(galleryThumbnails[nextIdx]);
+}
+function showGalleryPrevItem() {
+  let currIdx = Number(galleryStageEl.dataset.activeidx);
+  let prevIdx = currIdx - 1;
+
+  // handle edge cases
+  if (currIdx === galleryThumbnails.length - 1) {
+    galleryStageNextBtn.style.visibility = "visible";
+  }
+  if (currIdx === 1) {
+    galleryStagePrevBtn.style.visibility = "hidden";
+  }
+
+  previewGalleryItem(galleryThumbnails[prevIdx])
+}
+
+initGallery();
--- js/lightbox.js
+++ js/lightbox.js
@@ -1,4 +1,7 @@
-var lightBoxPrev = document.querySelector(".js-lightbox-preview");
+var lightBoxStageEl = document.querySelector(".jsLbStage");
+var lightBoxThumbNails = document.querySelectorAll(".jsLbItem");
+var lightBoxPrevBtn = document.querySelector('.jsLbStagePrev');
+var lightBoxNextBtn = document.querySelector('.jsLbStageNext');
 
 function openLightBox(dialogId, that) {
   if (!dialogId) {
@@ -7,7 +10,60 @@ function openLightBox(dialogId, that) {
   if (!that) {
     throw Error("context(`this`) is required");
   }
+
+  // pass gallery active img info to lightbox
+  currLightboxIdx = that.dataset.activeidx;
+
   let currImg = that.dataset.curr_img;
-  lightBoxPrev.src = "../images/" + currImg;
+  lightBoxStageEl.src = "../images/" + currImg;
+  setActiveThumbnail(lightBoxThumbNails, lightBoxThumbNails[0]);
+
   openDialog(dialogId, that);
 }
+
+function initLightBox() {
+
+  // disable nextPrevBtn if slide count = 1
+  if (lightBoxThumbNails.length < 2) {
+    lightBoxNextBtn.style.display = "none";
+    lightBoxPrevBtn.style.display = "none";
+  } else {
+    lightBoxNextBtn.setAttribute('onclick', "showLBNextItem(this)");
+    lightBoxPrevBtn.setAttribute('onclick', "showLBPrevItem(this)");
+  }
+  // add listeners
+  lightBoxThumbNails.forEach((thumb) => {
+    thumb.setAttribute("onclick", "previewLightBoxItem(this)");
+  })
+
+  // set inital data for stage
+  lightBoxStageEl.setAttribute("data-activeidx", "0");
+}
+
+function previewLightBoxItem(that) {
+  if (that.dataset.fullimageurl === lightBoxStageEl.dataset.fullimageurl) {
+    return;
+  }
+  lightBoxStageEl.setAttribute("data-curr_img", that.dataset.fullimageurl);
+  lightBoxStageEl.setAttribute("data-activeidx", that.dataset.idx);
+
+  lightBoxStageEl.src = "../images/" + that.dataset.fullimageurl;
+
+  setActiveThumbnail(lightBoxThumbNails, that);
+}
+function showLBNextItem() {
+  let currIdx = Number(lightBoxStageEl.dataset.activeidx);
+  let nextIdx = (currIdx + 1) % lightBoxThumbNails.length;
+  previewLightBoxItem(lightBoxThumbNails[nextIdx])
+}
+function showLBPrevItem() {
+  let currIdx = Number(lightBoxStageEl.dataset.activeidx);
+  let prevIdx;
+  if (currIdx === 0) {
+    prevIdx = lightBoxThumbNails.length - 1;
+  } else {
+    prevIdx = (currIdx - 1) % lightBoxThumbNails.length;
+  }
+  previewLightBoxItem(lightBoxThumbNails[prevIdx])
+}
+initLightBox();
