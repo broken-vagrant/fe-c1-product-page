c51c482edf76eb38b5462a3d29bf3108da7a91a7
zkindest <zkindest5@gmail.com>
2022-05-23 23:08:11 +0530

    Fix Menu and improve classnames

--- js/utils.js
+++ js/utils.js
@@ -1,10 +1,24 @@
-function setActiveThumbnail(thumbnails, targetThumbnail, activeClassName = "active") {
+function setActiveThumbnail(
+  thumbnails,
+  targetThumbnail,
+  activeClassName = "active"
+) {
   thumbnails.forEach((item) => {
     if (item.classList.contains(activeClassName)) {
-      item.classList.remove(activeClassName);
+      item.classList.remove(activeClassName)
     }
     if (item === targetThumbnail) {
       item.classList.add(activeClassName)
     }
   })
+}
+
+function debounce(func, timeout = 300) {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, timeout)
+  }
 }
